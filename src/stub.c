1c451cc234d9e0c2c05fb2f268d93aa37438140e
CanOzSec <canozsec@gmail.com>
2023-10-06 09:12:07 +0000

    Fixed xor function

--- src/stub.c
+++ src/stub.c
@@ -186,10 +186,9 @@ void XOR(char * data, size_t data_len, char * key, size_t key_len) {
     int j = 0;
 
     for (int i = 0; i < data_len; i++) {
-        if (j == key_len - 1) j = 0;
-
         data[i] = data[i] ^ key[j];
         j++;
+        if (j == key_len) j = 0;
     }
 }
 
